17cf381567a0a09213c52465b176e5addc4a4596
Tushar Koshti <koshtitushar1994@gmail.com>
2017-11-26 08:04:33 +0530

    Added nuget create config.

---
+++ CrossFileHelper/Abstractions/IFileHelper.cs
@@ -0,0 +1,25 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+
+namespace CrossFileHelper.Abstractions
+{
+	/// <summary>
+	/// File helper interface
+	/// </summary>
+	public interface IFileHelper
+    {
+		/// <summary>
+		/// Get file write stream async
+		/// </summary>
+		/// <param name="filePath">Path of file to write</param>
+		/// <returns>Stream</returns>
+		Task<Stream> GetFileWriteStreamAsync(string filePath);
+
+		/// <summary>
+		/// Get file read stream async
+		/// </summary>
+		/// <param name="filePath">Path of file to read</param>
+		/// <returns>Stream</returns>
+		Task<Stream> GetFileReadStreamAsync(string filePath);
+    }
+}
--- CrossFileHelper/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace CrossFileHelper
-{
-    public class Class1
-    {
-    }
-}
