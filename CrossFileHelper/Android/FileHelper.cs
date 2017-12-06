c12ac86114ca11b2972f1986a9103f3087fbe839
Tushar Koshti <koshtitushar1994@gmail.com>
2017-12-06 23:08:33 +0530

    Added multi target support.

--- CrossFileHelper/Android/FileHelper.cs
+++ CrossFileHelper/Android/FileHelper.cs
@@ -20,5 +20,54 @@ namespace CrossFileHelper.Android
 			FileStream fileStream = new FileStream(filePath, FileMode.Create, FileAccess.ReadWrite, FileShare.None);
 			return Task<Stream>.Factory.StartNew(() => fileStream);
 		}
+
+		//public async Task<string> PickFileAsync(string mimeType)
+		//{
+		//	if (!(await RequestStoragePermission()))
+		//	{
+		//		throw new FilePermissionException(Permission.Storage);
+		//	}
+		//	var media = await PickFileAsync(mimeType, Intent.ActionPick, null);
+
+		//	if (options == null)
+		//		options = new PickMediaOptions();
+
+		//	//check to see if we picked a file, and if so then try to fix orientation and resize
+		//	if (!string.IsNullOrWhiteSpace(media?.Path))
+		//	{
+		//		try
+		//		{
+		//			var originalMetadata = new ExifInterface(media.Path);
+
+		//			if (options.RotateImage)
+		//			{
+		//				await FixOrientationAndResizeAsync(media.Path, options, originalMetadata);
+		//			}
+		//			else
+		//			{
+		//				await ResizeAsync(media.Path, options.PhotoSize, options.CompressionQuality, options.CustomPhotoSize, originalMetadata);
+		//			}
+		//			if (options.SaveMetaData && IsValidExif(originalMetadata))
+		//			{
+		//				try
+		//				{
+		//					originalMetadata?.SaveAttributes();
+		//				}
+		//				catch (Exception ex)
+		//				{
+		//					Console.WriteLine($"Unable to save exif {ex}");
+		//				}
+		//			}
+
+		//			originalMetadata?.Dispose();
+		//		}
+		//		catch (Exception ex)
+		//		{
+		//			Console.WriteLine("Unable to check orientation: " + ex);
+		//		}
+		//	}
+
+		//	return media;
+		//}
 	}
 }
